dc873be4e2157d57933e9dd8ac6deb7b78b1d8c7
spencer davis <dsdavis6@ncsu.edu>
2024-03-20 03:42:21 +0000

    make it work

--- dataset_processing/tartan_air_to_bag.py
+++ dataset_processing/tartan_air_to_bag.py
@@ -11,7 +11,8 @@ import numpy as np
 
 folder = 'abandonedfactory_sample_P001'
 seq = 'P001'
-
+image_min = 0
+image_max = 433
 
 def main(args=None):
     writer = rosbag2_py.SequentialWriter()
@@ -22,42 +23,65 @@ def main(args=None):
     converter_options = rosbag2_py._storage.ConverterOptions('', '')
     writer.open(storage_options, converter_options)
 
-    topic_info = rosbag2_py._storage.TopicMetadata(
-        name='image',
-        type='sensor_msgs/msg/CompressedImage',
+    left_topic = rosbag2_py._storage.TopicMetadata(
+        name='image_left',
+        type='sensor_msgs/msg/Image',
         serialization_format='cdr')
-    writer.create_topic(topic_info)
+    writer.create_topic(left_topic)
 
-    img = Image.open('sample.png', mode='r', formats=None)
+    right_topic = rosbag2_py._storage.TopicMetadata(
+        name='image_right',
+        type='sensor_msgs/msg/Image',
+        serialization_format='cdr')
+    writer.create_topic(right_topic)
 
-    # rgb_img = img.convert('RGB')
 
-    output = io.BytesIO()
-    img.save(output, format='png')
-    hex_data = output.getvalue()
+    for i in range(image_min, image_max + 1):
+        image_name = '{:06d}_left.png'.format(i)
+        print(image_name)
+        img = Image.open(folder + '/' + seq + '/' +  'image_left/' + image_name, mode='r', formats=None)
 
-    msg = CompressedImage()
-    msg.format = 'png'
-    msg.data = hex_data
-    msg.header.frame_id = "camera"
-    
+        rgb_img = img.convert('RGB')
 
-    # msg = RosImage()
-    # #msg.header.stamp = rospy.Time.now()
-    # msg.height = rgb_img.height
-    # msg.width = rgb_img.width
-    # msg.encoding = "rgb8"
-    # msg.is_bigendian = False
-    # msg.step = 3 * rgb_img.width
-    # msg.data = np.array(rgb_img).tobytes()
+        msg = RosImage()
+        msg.height = rgb_img.height
+        msg.width = rgb_img.width
+        msg.encoding = "rgb8"
+        msg.is_bigendian = False
+        msg.step = 3 * rgb_img.width
+        msg.data = np.array(rgb_img).tobytes()
 
-    for i in range(0, 1000):
-        ns = (int) (i * 1000000 * 16.6)
+        ns = (int) (i * 1000000 * 33.33333)
         t = Time(seconds=0, nanoseconds=ns)
         msg.header.stamp = t.to_msg()
 
         writer.write(
-                'image',
+                'image_left',
+                serialize_message(msg),
+                ns)
+
+
+    for i in range(image_min, image_max + 1):
+        image_name = '{:06d}_right.png'.format(i)
+        print(image_name)
+        img = Image.open(folder + '/' + seq + '/' +  'image_right/' + image_name, mode='r', formats=None)
+
+        rgb_img = img.convert('RGB')
+
+        msg = RosImage()
+        msg.height = rgb_img.height
+        msg.width = rgb_img.width
+        msg.encoding = "rgb8"
+        msg.is_bigendian = False
+        msg.step = 3 * rgb_img.width
+        msg.data = np.array(rgb_img).tobytes()
+
+        ns = (int) (i * 1000000 * 33.33333)
+        t = Time(seconds=0, nanoseconds=ns)
+        msg.header.stamp = t.to_msg()
+
+        writer.write(
+                'image_right',
                 serialize_message(msg),
                 ns)
 
