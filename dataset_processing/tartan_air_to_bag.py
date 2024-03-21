56229b09681c807c56909edd5b1f6389736e616e
spencer davis <dsdavis6@ncsu.edu>
2024-03-21 00:32:43 +0000

    extract pose gt

--- dataset_processing/tartan_air_to_bag.py
+++ dataset_processing/tartan_air_to_bag.py
@@ -3,18 +3,34 @@ from rclpy.serialization import serialize_message
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
 from sensor_msgs.msg import Image as RosImage
+from nav_msgs.msg import Odometry
+
 from PIL import Image
 import io
 import rosbag2_py
 from rclpy.time import Time
 import numpy as np
+import os
 
-folder = 'abandonedfactory_sample_P001'
-seq = 'P001'
-image_min = 0
-image_max = 433
+folder = 'abandonedfactory/Easy'
+seq = 'P000'
+framerate = 20
+
+def get_left_image_filename(idx):
+    image_name = '{:06d}_left.png'.format(idx)
+    name = folder + '/' + seq + '/image_left/' + image_name
+    return name
+
+def get_right_image_filename(idx):
+    image_name = '{:06d}_right.png'.format(idx)
+    name = folder + '/' + seq + '/image_right/' + image_name
+    return name
+
 
 def main(args=None):
+    img_count = len(os.listdir(folder + '/' + seq + '/image_right/'))
+    print(f'{img_count} images found in sequence ' + seq + ' of ' + folder)
+
     writer = rosbag2_py.SequentialWriter()
 
     storage_options = rosbag2_py._storage.StorageOptions(
@@ -35,12 +51,22 @@ def main(args=None):
         serialization_format='cdr')
     writer.create_topic(right_topic)
 
+    odom_left_topic = rosbag2_py._storage.TopicMetadata(
+        name='odom_left',
+        type='nav_msgs/msg/Odometry',
+        serialization_format='cdr')
+    writer.create_topic(odom_left_topic)
 
-    for i in range(image_min, image_max + 1):
-        image_name = '{:06d}_left.png'.format(i)
-        print(image_name)
-        img = Image.open(folder + '/' + seq + '/' +  'image_left/' + image_name, mode='r', formats=None)
+    odom_right_topic = rosbag2_py._storage.TopicMetadata(
+        name='odom_right',
+        type='nav_msgs/msg/Odometry',
+        serialization_format='cdr')
+    writer.create_topic(odom_right_topic)
 
+    for i in range(0, img_count):
+        filename = get_left_image_filename(i)
+        print(filename)
+        img = Image.open(filename, mode='r', formats=None)
         rgb_img = img.convert('RGB')
 
         msg = RosImage()
@@ -51,7 +77,7 @@ def main(args=None):
         msg.step = 3 * rgb_img.width
         msg.data = np.array(rgb_img).tobytes()
 
-        ns = (int) (i * 1000000 * 33.33333)
+        ns = (int) (i * 1000000 * (1/framerate) * 1000)
         t = Time(seconds=0, nanoseconds=ns)
         msg.header.stamp = t.to_msg()
 
@@ -61,11 +87,10 @@ def main(args=None):
                 ns)
 
 
-    for i in range(image_min, image_max + 1):
-        image_name = '{:06d}_right.png'.format(i)
-        print(image_name)
-        img = Image.open(folder + '/' + seq + '/' +  'image_right/' + image_name, mode='r', formats=None)
-
+    for i in range(0, img_count):
+        filename = get_right_image_filename(i)
+        print(filename)
+        img = Image.open(filename, mode='r', formats=None)
         rgb_img = img.convert('RGB')
 
         msg = RosImage()
@@ -76,7 +101,7 @@ def main(args=None):
         msg.step = 3 * rgb_img.width
         msg.data = np.array(rgb_img).tobytes()
 
-        ns = (int) (i * 1000000 * 33.33333)
+        ns = (int) (i * 1000000 * (1/framerate) * 1000)
         t = Time(seconds=0, nanoseconds=ns)
         msg.header.stamp = t.to_msg()
 
@@ -85,6 +110,72 @@ def main(args=None):
                 serialize_message(msg),
                 ns)
 
+    left_camera_gt = np.loadtxt(folder + '/' + seq + '/pose_left.txt')
+    right_camera_gt = np.loadtxt(folder + '/' + seq + '/pose_right.txt')
+    
+    print('Writing left camera ground truth')
+    for i in range(0, img_count):
+        msg = Odometry()
+        ns = (int) (i * 1000000 * (1/framerate) * 1000)
+        t = Time(seconds=0, nanoseconds=ns)
+        msg.header.stamp = t.to_msg()
+        msg.header.frame_id = 'map'
+        msg.child_frame_id = 'left_camera'
+
+        x = left_camera_gt[i][0]
+        y = left_camera_gt[i][1]
+        z = left_camera_gt[i][2]
+
+        qx = left_camera_gt[i][3]
+        qy = left_camera_gt[i][4]
+        qz = left_camera_gt[i][5]
+        qw = left_camera_gt[i][6]
+
+        msg.pose.pose.position.x = x
+        msg.pose.pose.position.y = y
+        msg.pose.pose.position.z = z
+
+        msg.pose.pose.orientation.x = qx
+        msg.pose.pose.orientation.y = qy
+        msg.pose.pose.orientation.z = qz
+        msg.pose.pose.orientation.w = qw
+
+        writer.write(
+                'odom_left',
+                serialize_message(msg),
+                ns)
+        
+    print('Writing right camera ground truth')
+    for i in range(0, img_count):
+        msg = Odometry()
+        ns = (int) (i * 1000000 * (1/framerate) * 1000)
+        t = Time(seconds=0, nanoseconds=ns)
+        msg.header.stamp = t.to_msg()
+        msg.header.frame_id = 'map'
+        msg.child_frame_id = 'right_camera'
+
+        x = right_camera_gt[i][0]
+        y = right_camera_gt[i][1]
+        z = right_camera_gt[i][2]
+
+        qx = right_camera_gt[i][3]
+        qy = right_camera_gt[i][4]
+        qz = right_camera_gt[i][5]
+        qw = right_camera_gt[i][6]
+
+        msg.pose.pose.position.x = x
+        msg.pose.pose.position.y = y
+        msg.pose.pose.position.z = z
+
+        msg.pose.pose.orientation.x = qx
+        msg.pose.pose.orientation.y = qy
+        msg.pose.pose.orientation.z = qz
+        msg.pose.pose.orientation.w = qw
+
+        writer.write(
+                'odom_right',
+                serialize_message(msg),
+                ns)
 
 
 if __name__ == '__main__':
