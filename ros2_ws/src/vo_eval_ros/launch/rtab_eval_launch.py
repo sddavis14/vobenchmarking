5a3cc4548a464cab52a05f351e77fb9193915f37
Spencer Davis <dsdavis6@ncsu.edu>
2024-03-28 21:58:22 +0000

    fix

--- ros2_ws/src/vo_eval_ros/launch/rtab_eval_launch.py
+++ ros2_ws/src/vo_eval_ros/launch/rtab_eval_launch.py
@@ -1,20 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import launch_ros
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package='rtabmap_odom',
-            executable='stereo_odometry',
-            name='stereo_odometry_node',
-            remappings=[
-                ('left/image_rect', 'camera_left/image_rect'),
-                ('left/camera_info', 'camera_left/camera_info'),
-                ('right/image_rect', 'camera_right/image_rect'),
-                ('right/camera_info', 'camera_right/camera_info'),
-            ],
-            parameters=['/home/spencer/vobenchmarking/ros2_ws/src/vo_eval_ros/config/odometer_params.yml'],
-        ),
+        launch_ros.actions.SetParameter(name='use_sim_time', value=True),
         Node(
             package='image_transport',
             executable='republish',
@@ -38,15 +28,34 @@ def generate_launch_description():
                        '--remap', 'out:=/camera_right/image_rect' ]
         ),
         Node(
-            package='tf2_ros', 
-            executable='static_transform_publisher', 
-            arguments=['-0.125', '0', '0', '0', '0', '0', 'base_link', 'camera_left'], 
+            package='rtabmap_odom',
+            executable='stereo_odometry',
+            name='stereo_odometry_node',
+            remappings=[
+                ('left/image_rect', '/camera_left/image_rect'),
+                ('left/camera_info', '/camera_left/camera_info'),
+                ('right/image_rect', '/camera_right/image_rect'),
+                ('right/camera_info', '/camera_right/camera_info'),
+            ],
+            parameters=['/home/ubuntu/vobenchmarking/ros2_ws/src/vo_eval_ros/config/odometer_params.yml'],
             output='screen'
         ),
         Node(
             package='tf2_ros', 
             executable='static_transform_publisher', 
-            arguments=['0.125', '0', '0', '0', '0', '0', 'base_link', 'camera_right'], 
+            arguments=['-0.125', '0', '0', '0', '0', '0', 'base_link', 'left_camera',  ], 
+            output='screen'
+        ),
+        Node(
+            package='tf2_ros', 
+            executable='static_transform_publisher', 
+            arguments=['0.125', '0', '0', '0', '0', '0', 'base_link', 'right_camera',  ], 
+            output='screen'
+        ),
+        Node(
+            package='rviz2', 
+            executable='rviz2', 
+            arguments=[], 
             output='screen'
         ),
         # Node(
